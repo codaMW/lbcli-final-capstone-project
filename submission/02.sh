22fd0b1f031a70f5feb8e1c4f248acfcad3a5768
codaMW <codamw011@gmail.com>
2025-04-24 17:58:23 +0000

    AI

--- submission/02.sh
+++ submission/02.sh
@@ -1,7 +1,9 @@
 # How many new outputs were created by block 243,825?
 
-block_hash=$(bitcoin-cli -signet getblockhash 243825)
+#block_hash=$(bitcoin-cli -signet getblockhash 243825)
 
-outputs=$(bitcoin-cli -signet getblock $block_hash | jq '.nTx')
+#outputs=$(bitcoin-cli -signet getblock $block_hash | jq '.nTx')
 
-echo "$outputs"
+#echo "$outputs"
+
+bitcoin-cli -signet getblockstats 243825 | jq -r '.outs'
